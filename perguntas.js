9200498118dd8a0699d7c3b1ba13d8d2efd4e710
stezinha01 <168651369+stezinha01@users.noreply.github.com>
2024-11-11 12:31:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual a formula de Bhaskara?',
+    '(-b±√(b²-4ac))/(2a)'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Inglaterra?',
+    'A capital da Inglaterra é Londres.'
+)
+
+criaCartao(
+    'Português',
+    'Qual a pontuação utilizada no final de uma frase?',
+    'O ponto final. (.)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz obrigado em Inglês?',
+    'Obrigado em ingles é Thank you.'
+)
